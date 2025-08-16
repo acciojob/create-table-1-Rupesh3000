1dd6016dbe12b1cd92d3cf7ff115cba46f14de95
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-08-16 22:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+  const sampleTable = document.getElementById("sampleTable");
+  sampleTable.insertBefore(newElemnt(), sampleTable.firstChild);
 }
+
+const newElemnt = () => {
+  const tr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  td1.innerHTML = "New Cell1";
+
+  const td2 = document.createElement("td");
+  td2.innerHTML = "New Cell2";
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  return tr;
+};
